0d2502f569b6397008883c4a5bccf77eb7106139
Olivier <maestun@free.fr>
2017-08-18 15:37:17 +0200

    nicer UI, multi file upload, images ok :p

--- ActionViewController.m
+++ ActionViewController.m
@@ -6,6 +6,9 @@
 //
 //
 
+
+// TODO: use framework or common code w/ app
+
 #import "ActionViewController.h"
 #import <MobileCoreServices/MobileCoreServices.h>
 
@@ -17,7 +20,8 @@
 
 
 @interface ActionViewController () {
-    NSURL * mFileURL;
+    NSMutableArray<NSURL *> * mFileURLs;
+    NSUInteger mTotalSize;
 }
 @property(strong,nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UITableView *tvMain;
@@ -27,6 +31,7 @@
 @property (weak, nonatomic) IBOutlet UIView *uvUploading;
 @property (weak, nonatomic) IBOutlet UILabel *lbUploading;
 @property (weak, nonatomic) IBOutlet UILabel *lbFileName;
+@property (weak, nonatomic) IBOutlet UILabel *lbPercent;
 @property (weak, nonatomic) IBOutlet UIProgressView *pvProgress;
 @end
 
@@ -48,11 +53,9 @@
     [[self tvMain] setDataSource:self];
     
     
-    // Get the item[s] we're handling from the extension context.
+    mFileURLs = [NSMutableArray new];
     
-    // For example, look for an image and place it into an image view.
-    // Replace this with something appropriate for the type[s] your extension supports.
-    BOOL imageFound = NO;
+    // TODO: write moar elegant code :p
     for (NSExtensionItem *item in self.extensionContext.inputItems) {
         for (NSItemProvider *itemProvider in item.attachments) {
             
@@ -60,49 +63,20 @@
                 
                 [itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeFileURL options:kNilOptions completionHandler:^(id<NSSecureCoding>  _Nullable item, NSError * _Null_unspecified error) {
                     if(error == nil) {
-                        
-                        mFileURL = (NSURL *)item;
-                        
-                        NSDictionary * attrs = [[NSFileManager defaultManager] attributesOfItemAtPath:[mFileURL relativePath] error:&error];
-                        NSLog(@"%@", attrs);
-                        
-                        
-//                        UIWebView * webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
-//                        NSString *urlString = @"cozydrive://test";
-//                        NSString * content = [NSString stringWithFormat : @"<head><meta http-equiv='refresh' content='0; URL=%@'></head>", urlString];
-//                        [webView loadHTMLString:content baseURL:nil];
-//                        [self.view addSubview:webView];
-//                        [webView performSelector:@selector(removeFromSuperview) withObject:nil afterDelay:2.0];
-
-                        
-//                        [[self extensionContext] openURL:[NSURL URLWithString:url_str] completionHandler:^(BOOL success) {
-//                            NSLog(@"open url : %d", success);
-//                        }];
+                        NSURL * url = (NSURL *)item;
+                        [mFileURLs addObject:url];
                     }
                 }];
                 
             }
-            
-            
-            if ([itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeImage]) {
-                // This is an image. We'll load it, then place it in our image view.
-                __weak UIImageView *imageView = self.imageView;
-                [itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeImage options:nil completionHandler:^(UIImage *image, NSError *error) {
-                    if(image) {
-                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-                            [imageView setImage:image];
-                        }];
+            else if([itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeImage]) {
+                [itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeImage options:nil completionHandler:^(id<NSSecureCoding>  _Nullable item, NSError * _Null_unspecified error) {
+                    if(error == nil) {
+                        NSURL * url = (NSURL *)item;
+                        [mFileURLs addObject:url];
                     }
                 }];
-                
-                imageFound = YES;
-                break;
             }
-        }
-        
-        if (imageFound) {
-            // We only handle one image, so stop looking for more.
-            break;
         }
     }
 }
@@ -135,24 +109,42 @@
     
     NSString * token = [NSString stringWithFormat:@"Bearer %@", @"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhY2Nlc3MiLCJpYXQiOjE1MDI0NjU3ODAsImlzcyI6Inpsb3QubXljb3p5LmNsb3VkIiwic3ViIjoiYWI4MDY0Zjc4OTE4OWY5ODE4YjVjZTUxMzBkZWUxODMiLCJzY29wZSI6ImlvLmNvenkuZmlsZXMgaW8uY296eS5jb250YWN0cyBpby5jb3p5LmpvYnM6UE9TVDpzZW5kbWFpbDp3b3JrZXIifQ.A3p_Qdn1Ky4fCmxyINgknJ0_mIUvPbyoBZu3-XMmdLlV5spcXV_v9b203ytSqGrUDBNmA_w40rUBJC0Xo_NyEw"];
     
-    NSString * file_name = [mFileURL lastPathComponent];
-    NSString * url_str = [NSString stringWithFormat:@"%@/files/io.cozy.files.root-dir?Type=file&Name=%@&Tags=file&Executable=false", @"https://zlot.mycozy.cloud", file_name];
+    NSString * base_url = @"https://zlot.mycozy.cloud";
+    
+    // TODO: create queue instead of several // reqs ?
+    mTotalSize = 0;
+    for(NSURL * url in mFileURLs) {
+        NSDictionary * attrs = [[NSFileManager defaultManager] attributesOfItemAtPath:[url relativePath] error:nil];
+        mTotalSize += [[attrs objectForKey:NSFileSize] unsignedIntegerValue];
+    }
     
     
-    NSURLSessionConfiguration * config = [NSURLSessionConfiguration defaultSessionConfiguration];
-    NSURLSession * session = [NSURLSession sessionWithConfiguration:config delegate:self delegateQueue:[NSOperationQueue mainQueue]];
+    for(NSURL * url in mFileURLs) {
+       NSString * file_name = [url lastPathComponent];
+        NSString * url_str = [NSString stringWithFormat:@"%@/files/io.cozy.files.root-dir?Type=file&Name=%@&Tags=file&Executable=false", base_url, file_name];
+        
+        
+        // TODO: use background ?
+        NSURLSessionConfiguration * config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        NSURLSession * session = [NSURLSession sessionWithConfiguration:config delegate:self delegateQueue:[NSOperationQueue mainQueue]];
+        
+        NSMutableURLRequest * request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url_str]];
+        [request setHTTPMethod: @"POST"];
+        
+        [request setValue:[self getMimeTypeFromPath:file_name] forHTTPHeaderField:@"Content-Type"];
+        [request setValue:token forHTTPHeaderField:@"Authorization"];
+        
+        NSURLSessionUploadTask * task = [session uploadTaskWithRequest:request fromFile:url];
+        [task resume];
+    }
     
-    NSMutableURLRequest * request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url_str]];
-    [request setHTTPMethod: @"POST"];
-    
-    [request setValue:[self getMimeTypeFromPath:file_name] forHTTPHeaderField:@"Content-Type"];
-    [request setValue:token forHTTPHeaderField:@"Authorization"];
-    
-    NSURLSessionUploadTask * task = [session uploadTaskWithRequest:request fromFile:mFileURL];
-    [task resume];
-
-    
-    [[self lbFileName] setText:file_name];
+    if([mFileURLs count] > 1) {
+        [[self lbFileName] setText:[NSString stringWithFormat:@"%ld files", [mFileURLs count]]];
+    }
+    else {
+        NSURL * url = [mFileURLs objectAtIndex:0];
+        [[self lbFileName] setText:[url lastPathComponent]];
+    }
     [[self pvProgress] setProgress:0.0];
     [self showProgress:YES];
 }
@@ -204,8 +196,16 @@
 #pragma mark - NSURLSessionTaskDelegate
 // ====================================================================================================================
 - (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didSendBodyData:(int64_t)bytesSent totalBytesSent:(int64_t)totalBytesSent totalBytesExpectedToSend:(int64_t)totalBytesExpectedToSend {
-    NSLog(@"didSendBodyData: send %lld / %lld", totalBytesSent, totalBytesExpectedToSend);
-    [[self pvProgress] setProgress:(double)totalBytesExpectedToSend / (double)totalBytesSent];
+    NSLog(@"didSendBodyData: send %lld / %lld", totalBytesSent, (int64_t)mTotalSize);
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        float val = (float)((float)totalBytesSent / (float)mTotalSize);
+        [[self pvProgress] setProgress:val];
+        NSLog(@"%f", val);
+        val *= 100.0;
+        NSLog(@"%d", (int)val);
+        [[self lbPercent] setText:[NSString stringWithFormat:@"%d %%", (int)val]];
+    });
 }
 
 
@@ -238,9 +238,8 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    return [mFileURLs count];
 }
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
     return @"File";
@@ -257,17 +256,22 @@
     if(cell == nil) {
         cell = [[FileCell alloc] initWithStyle:(UITableViewCellStyleDefault) reuseIdentifier:cell_id];
     }
-    [[cell lbName] setText:[mFileURL lastPathComponent]];
+    
+    NSURL * url = [mFileURLs objectAtIndex:[indexPath row]];
+    [[cell lbName] setText:[url lastPathComponent]];
+    
+    // try loading image lol
+    UIImage * img = [UIImage imageWithContentsOfFile:[url relativePath]];
+    if(img) {
+        [[cell ivIcon] setImage:img];
+    }
+    
+    
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
 }
-
-
-
-
-
 
 @end
